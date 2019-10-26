0e0164c09cdf2116734ad0e8e385dcbd7957d297
Emiel Van Severen <emielvanseveren@outlook.com>
2019-10-26 20:36:31 +0000

    Responsive <Hamburger/>

--- frontend/src/components/layout/Hamburger.jsx
+++ frontend/src/components/layout/Hamburger.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import size from '../../constants/size'
 
 const Container = styled.div`
   position: absolute;
@@ -9,13 +10,26 @@ const Container = styled.div`
   align-items: center;
   right: 100px;
   top: 100px;
+  z-index: 3;
   cursor: pointer;
+
+  @media ${size.xs}{
+    top: 75px;
+    right: 30px;
+  }
+
 `
 const Line = styled.div`
   width: 15px;
   height: 2px;
   margin-bottom: 2px;
   background-color: ${({ inRange }) => inRange ? 'white' : 'black'};
+
+  @media ${size.xs}{
+    width: 20px;
+    height: 3px;
+    margin-bottom: 3px;
+  }
 `
 
 export default function Hamburger({ inRange }){
