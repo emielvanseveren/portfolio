73dc2c9aed7d46778449d99b0715cf2823ed53b7
Emiel Van Severen <emielvanseveren@outlook.com>
2019-10-27 13:59:24 +0000

    Fix koa server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,22 +2,12 @@ require('dotenv').config()
 const Koa = require('koa')
 const app = new Koa()
 // modules
-const Router = require('koa-router')
-const router = new Router()
 const serve = require('koa-static')
-const BodyParser = require('koa-bodyparser')
 const logger = require('koa-logger')
-const mount = require('koa-mount')
 
-const staticPages = new Koa()
-staticPages.use(serve(__dirname + '/../frontend/dist'))
-app.use(mount('/', staticPages))
 
-app.use(BodyParser())
-app.use(logger())
-
-app.use(router.routes())
-  .use(router.allowedMethods())
+app.use(logger()) // automatically logs routes
+app.use(serve(__dirname + '/../../frontend/dist'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log('listening on port %s', PORT))
